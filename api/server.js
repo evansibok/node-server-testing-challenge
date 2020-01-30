90d763aa6fbb5a662c829190859375a86686755f
Evans Ibok <evansibok@gmail.com>
2020-01-30 15:33:52 +0100

    Setup Test configuration

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const routes = require('./routes');
+const routes = require('./baseRoute');
 const rootMiddleware = require('../middlewares');
 
 const server = express();
