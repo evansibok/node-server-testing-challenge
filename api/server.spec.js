e42dda59a7a6bb81d6d4073a64c223244b0e3e05
Evans Ibok <evansibok@gmail.com>
2020-01-30 16:48:09 +0100

    Add test for endpoint header

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,4 +6,15 @@ describe('Server file', () => {
     expect(process.env.DB_ENV).toBe('testing');
   })
   
+  describe("Main '/api/' endpoint works", () => {
+    test('should return 200 OK using supertest', () => {
+      request(server).get('/')
+      .expect(200);
+    })
+
+    test('should return the right header', () => {
+      request(server).get('/')
+        .expect('Content-Length', '14');
+    })
+  })
 })
